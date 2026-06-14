30f0bdcef8252441d828d3620aa4f4435873b6d4
rukyasu <notifyme+git@rbox.me>
2026-06-14 17:22:49 +0200

    Bash tweaks

--- bash/dot_bash/settings.bash
+++ bash/dot_bash/settings.bash
@@ -1,6 +1,10 @@
-HISTSIZE=1048576
-HISTFILE="$HOME/.bash_history"
+HISTSIZE=10000
 SAVEHIST=$HISTSIZE
+HISTFILE="$HOME/.bash_history"
+
+# don't put duplicate lines or lines starting with space in the history.
+HISTCONTROL=ignoreboth
+
 shopt -s histappend # append to history file
 
 export EDITOR=vim
