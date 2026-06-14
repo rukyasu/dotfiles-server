e739d999939bc9367c7a15d79cc75bcb03fecb5b
rukyasu <notifyme+git@rbox.me>
2026-06-14 18:02:32 +0200

    Fix bash prompt colours

--- bash/dot_bash/prompt.bash
+++ bash/dot_bash/prompt.bash
@@ -1,3 +1,8 @@
+# set variable identifying the chroot you work in (used in the prompt below)
+if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
+    debian_chroot=$(cat /etc/debian_chroot)
+fi
+
 case "$TERM" in
     xterm-color|*-256color) color_prompt=yes;;
 esac
@@ -14,3 +19,11 @@ if [ -n "$force_color_prompt" ]; then
         color_prompt=
     fi
 fi
+
+if [ "$color_prompt" = yes ]; then
+    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+else
+    PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+fi
+
+unset color_prompt force_color_prompt
